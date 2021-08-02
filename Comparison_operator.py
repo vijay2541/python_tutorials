05e459a05131cb84b99d485b8162bd79f5a8b2d2
vijay2541 <vjsh2541@gmail.com>
2021-08-02 22:21:39 +0530

    Comparators

---
+++ Comparison_operator.py
@@ -0,0 +1,10 @@
+x = 3 <= 2
+y = 9 == 8
+z = 4 != 7
+
+print(x)
+print(y)
+print(z)
+
+# < (lessthan)  /  <= (lesstha equal )  /  > (greaterthan)  /  >= (greaterthan equal)
+# == (equal to) /  != (not equal)
