76322dfbf90126574cffcebc543b87a148143150
vijay2541 <vjsh2541@gmail.com>
2021-08-02 21:29:02 +0530

    Basic operations on string

---
+++ Strings.py
@@ -0,0 +1,7 @@
+course = "Python for beginners"
+print(course.upper())                   #will convert complete string to uppercase
+print(course.lower())                   #will convert complete string to lowercase
+print(course.replace("for", "4"))       #will replace "for" with "4"
+print(course.find("for"))               #return the index of first alphabet of "for"
+print("Python" in course)               #re turn boolen value is given string "python" is present in variable course
+print(course)                           #The original string will not be mutate
