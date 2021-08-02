041a1dbaa9465d9b57638d8d8f7fbe8ce6497368
vijay2541 <vjsh2541@gmail.com>
2021-08-02 21:54:41 +0530

    Basic arithmetic operations

---
+++ Arithmetic_operations.py
@@ -0,0 +1,12 @@
+x = 10
+print(x + 3)
+print(x - 3)
+print(x / 3)
+print(x // 3)           #return whole number after devision
+print(x % 3)            #return remainder
+print(x * 3)
+print(x ** 3)           #10 to the power 3
+x += 3                  #incriment by 3
+print(x)
+x -= 5                  #decriment by 5
+print(x)
